cbd865601c0efc86c03e5da96e9598c9f2bd16d4
github username <github email address>
2018-06-15 06:23:24 -0400

    generate users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
   get 'signin', to: "sessions#new"
   post "sessions/create", to: "sessions#create"
   root 'static_pages#home'
