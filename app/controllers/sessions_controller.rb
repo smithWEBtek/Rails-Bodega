e5da8ace30ce2bea76281413df928f03a84c9b33
github username <github email address>
2018-06-15 20:28:29 -0400

    updates user vies and create purchases views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,21 @@
 class SessionsController < ApplicationController
   def new
     @user = User.new
+    @user = User.all
   end
 
   def create
-    user = User.find_by(name: params[:user][:name])
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: "Welcome to The Bodega!!"
+    else
+      redirect_to signin_path
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'signin', to: "sessions#new"
-  post "sessions/create", to: "sessions#create"
   root 'static_pages#home'
+  get 'signin', to: "sessions#new"
+  delete '/signout', to: "session#destroy"
+  post "sessions/create", to: "sessions#create"
+  resources :users
+  resources :purchases
 end
