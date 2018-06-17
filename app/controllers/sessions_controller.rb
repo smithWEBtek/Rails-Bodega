3bc7a5365dac24fee531cf921951af3ae9916809
github username <github email address>
2018-06-17 08:56:01 -0400

    Add links to site navigation bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,9 @@
 class SessionsController < ApplicationController
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
   def new
     @user = User.new
     @users = User.all
@@ -12,10 +17,5 @@ class SessionsController < ApplicationController
     else
       redirect_to signin_path
     end
-  end
-
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'signin', to: "sessions#new"
   post "/sessions/create", to: "sessions#create"
-  delete '/signout', to: "session#destroy"
+  delete '/signout', to: "sessions#destroy"
   post "/items/new", to: "items#new"
   resources :users
   resources :purchases
