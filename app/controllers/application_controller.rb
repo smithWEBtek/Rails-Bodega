a1e66400f69c52338221801bc926c9207578abc1
github username <github email address>
2018-06-17 15:44:45 -0400

    update purchases index page to show specific links when you're the admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :current_user
   before_action :require_logged_in, except: [:new, :create, :home]
 
