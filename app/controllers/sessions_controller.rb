a52c49424750df8fbae4ab869a856f1f8cd5efaf
github username <github email address>
2018-06-20 07:09:14 -0400

    update sessions#create to add omniauth steps and update the view to add login through facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,12 +10,25 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
-    if @user && @user.authenticate(params[:user][:password])
+    if auth
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.name = auth['info']['name']
+      end
       session[:user_id] = @user.id
       redirect_to user_path(@user), notice: "Welcome to The Bodega!!"
+    elsif
+      @user = User.find_by(name: params[:user][:name])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user), notice: "Welcome to The Bodega!!"
     else
       redirect_to signin_path
     end
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
