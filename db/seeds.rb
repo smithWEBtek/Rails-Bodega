f2128e84e1b69eedbcbe81603b0f14c575fc2206
github username <github email address>
2018-06-27 19:40:27 -0400

    add seed data

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Purchase.delete_all
+Purchase.create(name: "Doritos", price: 1.0, quantity: 10)
+Purchase.create(name: "Paper Towels 6 Rolls", price: 10.0, quantity: 10)
+Purchase.create(name: "Tide Liquid Detergent", price: 15.0, quantity: 10)
+Purchase.create(name: "Snickers Minis", price: 4.0, quantity: 10)
+Purchase.create(name: "Tortilla Chips", price: 4.0, quantity: 10)
+Purchase.create(name: "Oatly Oat Milk", price: 5.0, quantity: 10)
