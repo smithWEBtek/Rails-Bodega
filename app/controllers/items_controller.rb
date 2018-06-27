794e0b3eba5356426e0e52f509bfcf2ffc9f1d0e
github username <github email address>
2018-06-27 19:19:04 -0400

    add order items migration and model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   def new
+    # This doesn't save the item
     @item = Item.create(
       :user_id => params[:user_id],
       :purchase_id => params[:purchase_id]
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,11 @@ class Item < ActiveRecord::Base
   belongs_to :cart
 
   def buy_item
+    # This uses the other methods to let the user know if they can buy the item and updates the purchases info
     enough_money, still_in_stock = meet_requirements
     if enough_money && still_in_stock
       update_puchase
+      self.cart << self.purchase
     elsif still_in_stock && !enough_money
       "Sorry. " + not_enough_money
     elsif enough_money && !still_in_stock
@@ -16,6 +18,7 @@ class Item < ActiveRecord::Base
 
 
   def meet_requirements
+    # Uses the join table to test if the user has enough money to buy the purchase.
     enough_money, still_in_stock = false
     if self.user.money >= self.purchase.price
       enough_money = true
@@ -27,6 +30,7 @@ class Item < ActiveRecord::Base
   end
 
   def update_puchase
+    # Update that properties of a Purchase and save it
     new_quantity = self.purchase.quantity - 1
     new_money = self.user.money - self.purchase.price
     self.user.update(
@@ -38,14 +42,21 @@ class Item < ActiveRecord::Base
   end
 
   def not_enough_money
+    # If you don't have enough money,
     "You don't have enough money to buy #{self.purchase.name}"
   end
 
   def no_more_in_stock
+    # If the purchase isn't in stock you can't buy it.
     "Sorry, there are no more #{self.purchase.name} in stock"
   end
 
-  def add_to_cart
-    
+  def cart
+    self.cart
   end
+
+  def add_to_cart
+    cart << self.purchase
+  end
+
 end
