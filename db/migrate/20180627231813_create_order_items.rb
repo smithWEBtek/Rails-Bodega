1556f8fd468a183d6b1d7b8d2d8df818562dd506
github username <github email address>
2018-06-27 19:35:25 -0400

    Add name to OrdersItem migration

--- db/migrate/20180627231813_create_order_items.rb
+++ db/migrate/20180627231813_create_order_items.rb
@@ -3,6 +3,7 @@ class CreateOrderItems < ActiveRecord::Migration[5.2]
     create_table :order_items do |t|
       t.belongs_to :purchase, foreign_key: true
       t.belongs_to :cart, foreign_key: true
+      t.string :name
       t.decimal :price
       t.decimal :quantity
 
